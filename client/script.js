04b0de047da7e7d2897c4cac8433ea2cf7b9eaf4
kpielacinski <52744959+kpielacinski@users.noreply.github.com>
2022-12-23 15:48:54 +0100

    bug fix

--- client/script.js
+++ client/script.js
@@ -41,7 +41,7 @@ function generateUniqueId() {
 
 function chatStripe(isAi, value, uniqueId) {
   return `
-    <div class="wrapper" ${isAi && "ai"}>
+    <div class="wrapper ${isAi && "ai"}">
       <div class="chat">
         <div class="profile">
           <img
